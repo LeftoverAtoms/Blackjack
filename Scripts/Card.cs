b2273301746c5e528d426eb2c5ea618f1e6ab57a
Adam Calvelage <adamjasoncalvelage@gmail.com>
2024-06-16 16:25:42 -0400

    Render Unique Cards

--- Scripts/Card.cs
+++ Scripts/Card.cs
@@ -1,9 +1,15 @@
-﻿namespace Blackjack
+﻿using Extensions;
+
+namespace Blackjack
 {
-    public struct Card
+    public readonly struct Card
     {
         public static CardSuit[] Suits { get; } = Enum.GetValues<CardSuit>();
         public static CardValue[] Values { get; } = Enum.GetValues<CardValue>();
+        public static char[] Symbols { get; } =
+        {
+            '♥', '♦', '♣', '♠',
+        };
 
         public string Name { get; }
         public CardSuit Suit { get; }
@@ -15,6 +21,171 @@
             Suit = suit;
             Value = value;
         }
+
+        public void Render()
+        {
+            ConsoleColor.Red.WriteLine(string.Format(ASCII.GetValueOrDefault(Value), Symbols[(int)Suit]));
+        }
+
+        private static Dictionary<CardValue, string> ASCII { get; } = new()
+        {
+            {
+                CardValue.Ace,
+                "╭───────────╮\r\n" +
+                "│A          │\r\n" +
+                "│           │\r\n" +
+                "│           │\r\n" +
+                "│     {0}     │\r\n" +
+                "│           │\r\n" +
+                "│           │\r\n" +
+                "│          A│\r\n" +
+                "╰───────────╯"
+            },
+            {
+                CardValue.Two,
+                "╭───────────╮\r\n" +
+                "│2          │\r\n" +
+                "│     {0}     │\r\n" +
+                "│           │\r\n" +
+                "│           │\r\n" +
+                "│           │\r\n" +
+                "│     {0}     │\r\n" +
+                "│          2│\r\n" +
+                "╰───────────╯"
+            },
+            {
+                CardValue.Three,
+                "╭───────────╮\r\n" +
+                "│3          │\r\n" +
+                "│     {0}     │\r\n" +
+                "│           │\r\n" +
+                "│     {0}     │\r\n" +
+                "│           │\r\n" +
+                "│     {0}     │\r\n" +
+                "│          3│\r\n" +
+                "╰───────────╯"
+            },
+            {
+                CardValue.Four,
+                "╭───────────╮\r\n" +
+                "│4          │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│           │\r\n" +
+                "│           │\r\n" +
+                "│           │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│          4│\r\n" +
+                "╰───────────╯"
+            },
+            {
+                CardValue.Five,
+                "╭───────────╮\r\n" +
+                "│5          │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│           │\r\n" +
+                "│     {0}     │\r\n" +
+                "│           │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│          5│\r\n" +
+                "╰───────────╯"
+            },
+            {
+                CardValue.Six,
+                "╭───────────╮\r\n" +
+                "│6          │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│           │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│           │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│          6│\r\n" +
+                "╰───────────╯"
+            },
+            {
+                CardValue.Seven,
+                "╭───────────╮\r\n" +
+                "│7          │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│     {0}     │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│           │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│          7│\r\n" +
+                "╰───────────╯"
+            },
+            {
+                CardValue.Eight,
+                "╭───────────╮\r\n" +
+                "│8          │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│     {0}     │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│     {0}     │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│          8│\r\n" +
+                "╰───────────╯"
+            },
+            {
+                CardValue.Nine,
+                "╭───────────╮\r\n" +
+                "│9          │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│     {0}     │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│          9│\r\n" +
+                "╰───────────╯"
+            },
+            {
+                CardValue.Ten,
+                "╭───────────╮\r\n" +
+                "│10         │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│   {0}   {0}   │\r\n" +
+                "│         10│\r\n" +
+                "╰───────────╯"
+            },
+            {
+                CardValue.Jack,
+                "╭───────────╮\r\n" +
+                "│J          │\r\n" +
+                "│           │\r\n" +
+                "│           │\r\n" +
+                "│     {0}     │\r\n" +
+                "│           │\r\n" +
+                "│           │\r\n" +
+                "│          J│\r\n" +
+                "╰───────────╯"
+            },
+            {
+                CardValue.Queen,
+                "╭───────────╮\r\n" +
+                "│Q          │\r\n" +
+                "│           │\r\n" +
+                "│           │\r\n" +
+                "│     {0}     │\r\n" +
+                "│           │\r\n" +
+                "│           │\r\n" +
+                "│          Q│\r\n" +
+                "╰───────────╯"
+            },
+            {
+                CardValue.King,
+                "╭───────────╮\r\n" +
+                "│K          │\r\n" +
+                "│           │\r\n" +
+                "│           │\r\n" +
+                "│     {0}     │\r\n" +
+                "│           │\r\n" +
+                "│           │\r\n" +
+                "│          K│\r\n" +
+                "╰───────────╯"
+            },
+        };
     }
 
     public enum CardSuit
--- Scripts/Program.cs
+++ Scripts/Program.cs
@@ -1,5 +1,4 @@
-﻿using Extensions;
-using System.Text;
+﻿using System.Text;
 
 namespace Blackjack
 {
@@ -7,158 +6,14 @@ namespace Blackjack
     {
         private static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+
             var deck = new Deck();
             deck.Shuffle();
 
-            deck.Draw();
-
-            char s = '♣';
-
-            Console.OutputEncoding = Encoding.Unicode;
-
-            ConsoleColor.DarkRed.WriteLine(
-                 "╭───────────╮\r\n" +
-                 "│K          │\r\n" +
-                $"│           │\r\n" +
-                $"│           │\r\n" +
-                $"│     {s}     │\r\n" +
-                $"│           │\r\n" +
-                $"│           │\r\n" +
-                 "│          K│\r\n" +
-                 "╰───────────╯");
-            Console.WriteLine();
-            ConsoleColor.DarkRed.WriteLine(
-                 "╭───────────╮\r\n" +
-                 "│Q          │\r\n" +
-                $"│           │\r\n" +
-                $"│           │\r\n" +
-                $"│     {s}     │\r\n" +
-                $"│           │\r\n" +
-                $"│           │\r\n" +
-                 "│          Q│\r\n" +
-                 "╰───────────╯");
-            Console.WriteLine();
-            ConsoleColor.DarkRed.WriteLine(
-                 "╭───────────╮\r\n" +
-                 "│J          │\r\n" +
-                $"│           │\r\n" +
-                $"│           │\r\n" +
-                $"│     {s}     │\r\n" +
-                $"│           │\r\n" +
-                $"│           │\r\n" +
-                 "│          J│\r\n" +
-                 "╰───────────╯");
-            Console.WriteLine();
-            ConsoleColor.DarkRed.WriteLine(
-                 "╭───────────╮\r\n" +
-                 "│10         │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                 "│         10│\r\n" +
-                 "╰───────────╯");
-            Console.WriteLine();
-            ConsoleColor.DarkRed.WriteLine(
-                 "╭───────────╮\r\n" +
-                 "│9          │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                $"│     {s}     │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                 "│          9│\r\n" +
-                 "╰───────────╯");
-            Console.WriteLine();
-            ConsoleColor.DarkRed.WriteLine(
-                 "╭───────────╮\r\n" +
-                 "│8          │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                $"│     {s}     │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                $"│     {s}     │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                 "│          8│\r\n" +
-                 "╰───────────╯");
-            Console.WriteLine();
-            ConsoleColor.DarkRed.WriteLine(
-                 "╭───────────╮\r\n" +
-                 "│7          │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                $"│     {s}     │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                $"│           │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                 "│          7│\r\n" +
-                 "╰───────────╯");
-            Console.WriteLine();
-            ConsoleColor.DarkRed.WriteLine(
-                 "╭───────────╮\r\n" +
-                 "│6          │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                 "│           │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                 "│           │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                 "│          6│\r\n" +
-                 "╰───────────╯");
-            Console.WriteLine();
-            ConsoleColor.DarkRed.WriteLine(
-                 "╭───────────╮\r\n" +
-                 "│5          │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                 "│           │\r\n" +
-                $"│     {s}     │\r\n" +
-                 "│           │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                 "│          5│\r\n" +
-                 "╰───────────╯");
-            Console.WriteLine();
-            ConsoleColor.DarkRed.WriteLine(
-                 "╭───────────╮\r\n" +
-                 "│4          │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                 "│           │\r\n" +
-                 "│           │\r\n" +
-                 "│           │\r\n" +
-                $"│   {s}   {s}   │\r\n" +
-                 "│          4│\r\n" +
-                 "╰───────────╯");
-            Console.WriteLine();
-            ConsoleColor.DarkRed.WriteLine(
-                 "╭───────────╮\r\n" +
-                 "│3          │\r\n" +
-                $"│     {s}     │\r\n" +
-                 "│           │\r\n" +
-                $"│     {s}     │\r\n" +
-                 "│           │\r\n" +
-                $"│     {s}     │\r\n" +
-                 "│          3│\r\n" +
-                 "╰───────────╯");
-            Console.WriteLine();
-            ConsoleColor.DarkRed.WriteLine(
-                 "╭───────────╮\r\n" +
-                 "│2          │\r\n" +
-                $"│     {s}     │\r\n" +
-                 "│           │\r\n" +
-                 "│           │\r\n" +
-                 "│           │\r\n" +
-                $"│     {s}     │\r\n" +
-                 "│          2│\r\n" +
-                 "╰───────────╯");
-            Console.WriteLine();
-            ConsoleColor.DarkRed.WriteLine(
-                 "╭───────────╮\r\n" +
-                 "│A          │\r\n" +
-                 "│           │\r\n" +
-                 "│           │\r\n" +
-                $"│     {s}     │\r\n" +
-                 "│           │\r\n" +
-                 "│           │\r\n" +
-                 "│          A│\r\n" +
-                 "╰───────────╯");
-            Console.WriteLine();
+            deck.Draw()?.Render();
+            deck.Draw()?.Render();
+            deck.Draw()?.Render();
 
             deck.Collect();
             deck.Shuffle();
